2c0bb7e6709935a2bae8eba1088c414aaf351eea
Gang Li <ligangty@users.noreply.github.com>
2023-02-17 15:35:25 +0800

    Add security related classes from old code base

---
+++ src/main/java/org/commonjava/indy/service/conf/KeycloakConfig.java
@@ -0,0 +1,47 @@
+package org.commonjava.indy.service.conf;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
+import io.smallrye.config.WithName;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.Optional;
+
+@ApplicationScoped
+@ConfigMapping( prefix = "keycloak" )
+public interface KeycloakConfig
+{
+    String KEYCLOAK_REALM = "keycloak.realm";
+
+    String KEYCLOAK_URL = "keycloak.url";
+
+    String KEYCLOAK_SERVER_RESOURCE = "keycloak.serverResource";
+
+    String KEYCLOAK_UI_RESOURCE = "keycloak.uiResource";
+
+    String KEYCLOAK_SERVER_CREDENTIAL_SECRET = "keycloak.serverCredentialSecret";
+
+    String KEYCLOAK_REALM_PUBLIC_KEY = "keycloak.realmPublicKey";
+
+    @WithName( "enabled" )
+    @WithDefault( "false" )
+    boolean isEnabled();
+
+    @WithName( "uiResource" )
+    Optional<String> uiResource();
+
+    @WithName( "serverResource" )
+    Optional<String> serverResource();
+
+    @WithName( "realmPublicKey" )
+    Optional<String> realmPublicKey();
+
+    @WithName( "uiJson" )
+    Optional<String> keycloakUiJson();
+
+    @WithName( "realm" )
+    Optional<String> realm();
+
+    @WithName( "url" )
+    Optional<String> url();
+}
