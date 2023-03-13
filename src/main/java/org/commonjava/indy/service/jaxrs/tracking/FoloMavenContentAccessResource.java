1f05f09f0ca087c91484f68f62a27c08b30556c1
Gang Li <ligangty@users.noreply.github.com>
2023-03-13 13:30:14 +0800

    Add intermediate REST APIs with service call client - Tracking

---
+++ src/main/java/org/commonjava/indy/service/jaxrs/tracking/FoloMavenContentAccessResource.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright (C) 2011-2022 Red Hat, Inc. (https://github.com/Commonjava/indy)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.commonjava.indy.service.jaxrs.tracking;
+
+import org.commonjava.indy.service.client.tracking.FoloMavenContentAccessServiceClient;
+import org.commonjava.indy.service.models.repository.StoreType;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameters;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.HEAD;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+import javax.ws.rs.core.UriInfo;
+
+import static org.commonjava.indy.service.client.Constants.CHECK_CACHE_ONLY;
+import static org.eclipse.microprofile.openapi.annotations.enums.ParameterIn.PATH;
+
+@Tag( description = "FOLO Tracked Content Access and Storage. Tracks retrieval and management of file/artifact content." )
+@Path( "/api/folo/track/{id}/maven/{type: (hosted|group|remote)}/{name}" )
+public class FoloMavenContentAccessResource
+{
+    @Inject
+    @RestClient
+    FoloMavenContentAccessServiceClient client;
+
+    @Operation(
+            description = "Store and track file/artifact content under the given artifact store (type/name) and path." )
+    @Parameters( { @Parameter( name = "id", description = "User-assigned tracking session key", in = PATH ),
+            @Parameter( name = "type", in = PATH, description = "The type of the repository.",
+                        content = @Content( schema = @Schema( implementation = StoreType.class ) ),
+                        required = true ) } )
+    @APIResponses( { @APIResponse( responseCode = "201", description = "Content was stored successfully" ),
+            @APIResponse( responseCode = "400",
+                          description = "No appropriate storage location was found in the specified store (this store, or a member if a group is specified)." ) } )
+    @PUT
+    @Path( "/{path: (.*)}" )
+    public Response doCreate( @PathParam( "id" ) final String id, @PathParam( "type" ) final String type,
+                              @PathParam( "name" ) final String name, @PathParam( "path" ) final String path,
+                              @Context final HttpServletRequest request, @Context final UriInfo uriInfo )
+    {
+        return client.doCreate( id, type, name, path, request, uriInfo );
+    }
+
+    @Operation(
+            description = "Store and track file/artifact content under the given artifact store (type/name) and path." )
+    @Parameters( { @Parameter( name = "id", description = "User-assigned tracking session key", in = PATH ),
+            @Parameter( name = "type", in = PATH, description = "The type of the repository.",
+                        content = @Content( schema = @Schema( implementation = StoreType.class ) ),
+                        required = true ) } )
+    @APIResponses( { @APIResponse( responseCode = "404", description = "Content is not available" ),
+            @APIResponse( responseCode = "200",
+                          description = "Header metadata for content (or rendered listing when path ends with '/index.html' or '/'" ), } )
+    @HEAD
+    @Path( "/{path: (.*)}" )
+    public Response doHead( @PathParam( "id" ) final String id, @PathParam( "type" ) final String type,
+                            @PathParam( "name" ) final String name, @PathParam( "path" ) final String path,
+                            @QueryParam( CHECK_CACHE_ONLY ) final Boolean cacheOnly,
+                            @Context final HttpServletRequest request, @Context final UriInfo uriInfo )
+    {
+        return client.doHead( id, type, name, path, cacheOnly, request, uriInfo );
+    }
+
+    @Operation(
+            description = "Retrieve and track file/artifact content under the given artifact store (type/name) and path." )
+    @Parameters( { @Parameter( name = "id", description = "User-assigned tracking session key", in = PATH ),
+            @Parameter( name = "type", in = PATH, description = "The type of the repository.",
+                        content = @Content( schema = @Schema( implementation = StoreType.class ) ),
+                        required = true ) } )
+    @APIResponses( { @APIResponse( responseCode = "404", description = "Content is not available" ),
+            @APIResponse( responseCode = "200",
+                          content = { @Content( schema = @Schema( implementation = String.class ) ),
+                                  @Content( schema = @Schema( implementation = StreamingOutput.class ) ) },
+                          description = "Rendered content listing (when path ends with '/index.html' or '/') or Content streaming for concrete files." ) } )
+    @GET
+    @Path( "/{path: (.*)}" )
+    public Response doGet( @PathParam( "id" ) final String id, @PathParam( "type" ) final String type,
+                           @PathParam( "name" ) final String name, @PathParam( "path" ) final String path,
+                           @Context final HttpServletRequest request, @Context final UriInfo uriInfo )
+    {
+        return client.doGet( id, type, name, path, request, uriInfo );
+    }
+
+}
